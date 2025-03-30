8a428ca9f0364667f558acad887d0543fb09f0d2
qcconnie <qcconnie@hotmail.com>
2025-03-30 00:16:55 +0000

    Update _naming.py

--- src/smc_benchmark/_naming.py
+++ src/smc_benchmark/_naming.py
@@ -12,3 +12,4 @@ VELOCITY = "v"  # velocity
 KIT_NAMING = {0: TIME, 2: FORCE, 3: DISPLACEMENT, 4: GAP}
 UT_NAMING = [TIME, DISPLACEMENT, FORCE, "L1", "L2"]  # L(VDT)1 & 2 not used
 KUL_NAMING = [TIME, DISPLACEMENT, FORCE]  # [s] , [mm], [kN]
+TUM_NAMING = [TIME, FORCE, GAP, DISPLACEMENT, TEMP]  # [s], [N], -1*[mm], [mm], [C]; recording started at GAP = 25mm
